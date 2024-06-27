be18a6df10c4381f12b1f22a4076963c54bcaabb
Romain <57217225+Albertosaur@users.noreply.github.com>
2024-06-27 17:53:03 +0100

    First commit, character movement and terrain

---
+++ Assets/Character/Scripts/CameraMultiController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+/// <summary>
+/// Unity Camera controller by Gaz Robinson
+/// 2020
+/// 
+/// Has both Orbit and Free functionality.
+/// </summary>
+
+public class CameraMultiController : MonoBehaviour
+{
+
+    [Header("Transforms to Control")]
+    [Tooltip("Should be the Character transform by default")]
+
+    public Transform followTarget = null;
+    public Vector3 focalOffset = new Vector3(0.0f, 0.0f, 0.0f);
+
+    [Header("Camera Options")]
+
+    [Range(0.1f, 100f)] public float orbitSensitivity = 20.0f;
+    [Range(0.1f, 1000f)] public float scrollSensitivity = 200.0f;
+
+    private Vector3 orbitAngles = new Vector3(37.5f, 45.0f, 0.0f);
+
+    public float viewDistance = 5.0f;
+
+    private bool active = false;
+
+    void Start()
+    {
+        OrbitUpdate();
+    }
+
+    void LateUpdate()
+    {
+        //Activate camera controls on mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+            UnityEngine.Cursor.visible = false;
+            active = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            UnityEngine.Cursor.lockState = CursorLockMode.None;
+            UnityEngine.Cursor.visible = true;
+            active = false;
+        }
+
+        viewDistance = Mathf.Clamp(viewDistance + Input.mouseScrollDelta.y * Time.deltaTime * -scrollSensitivity, 2.0f, 300.0f);
+
+        if (active)
+        {
+            OrbitUpdate();
+        }
+        transform.position = (followTarget ? followTarget.position : Vector3.zero) + focalOffset - transform.forward * viewDistance;
+
+    }
+    void OrbitUpdate()
+    {
+        orbitAngles.x -= Input.GetAxisRaw("Mouse Y") * Time.deltaTime * orbitSensitivity;
+        orbitAngles.x = Mathf.Clamp(orbitAngles.x, 0, 90);
+        orbitAngles.y -= Input.GetAxisRaw("Mouse X") * Time.deltaTime * -orbitSensitivity;
+
+        transform.localRotation = Quaternion.AngleAxis(orbitAngles.y, Vector3.up);
+        transform.localRotation *= Quaternion.AngleAxis(orbitAngles.x, Vector3.right);
+    }
+}
+
---
+++ Assets/Character/Scripts/CharacterMovement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    private GameObject thirdPersonCamera;
+    private Transform characterTransform;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer = true;
+    private float gravityValue = -9.81f;
+
+    [Header("Movement Options")]
+    [Range(0.1f, 10f)] public float playerSpeed = 2.0f;
+    [Range(0.1f, 10f)] public float jumpHeight = 1.0f;
+    [Range(0.1f, 10f)] public float sprintMultiplier = 2.0f;
+
+    private void Start()
+    {
+        thirdPersonCamera = Camera.main.gameObject;
+        controller = gameObject.AddComponent<CharacterController>();
+        characterTransform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        var yRotation = thirdPersonCamera.transform.localRotation.eulerAngles.y;
+
+        if (thirdPersonCamera.activeInHierarchy)
+        {
+            characterTransform.localRotation = Quaternion.Euler(0, yRotation, 0);
+        }
+
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3((thirdPersonCamera.transform.forward.x * Input.GetAxis("Vertical")), 0, (thirdPersonCamera.transform.forward.z * Input.GetAxis("Vertical")));
+            move += new Vector3((thirdPersonCamera.transform.right.x * Input.GetAxis("Horizontal")), 0, (thirdPersonCamera.transform.right.z * Input.GetAxis("Horizontal")));
+            controller.Move(move * Time.deltaTime * playerSpeed);
+        
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
